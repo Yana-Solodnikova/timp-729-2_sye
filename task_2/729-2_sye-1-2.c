75fd086962018251e302f0bd47b9bff0b6b7a459
Yana Solodnikova <solodnikova.01@mail.ru>
2021-05-04 16:18:52 +0700

    task_2 and task_3

---
+++ task_2/729-2_sye-1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    double a,b;
+    (void)scanf("%lf",&a);
+    (void)scanf("%lf",&b);
+    printf("%lf",a+b);
+    return 0;
+}
---
+++ task_3/729-2_sye-1-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a,b;
+    (void)scanf("%lf",&a);
+    (void)scanf("%lf",&b);
+    printf("%lf",pow(a,b));
+    return 0;
+}
+
